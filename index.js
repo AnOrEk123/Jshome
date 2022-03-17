fdab39ba57b777d2a1dde2ebaff0fc0d2bcf68ae
EgorTal <96070284+EgorTal@users.noreply.github.com>
2022-03-17 20:54:03 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+let number = prompt("Введите число?");
+switch (number) {
+  case '0':
+      alert( 'ноль' )
+      break;
+  case '1':
+    alert( 'Один' );
+    break;
+  case '2':
+    alert( 'Два' );
+    break;
+  case '3':
+    alert( 'Три' );
+    break;
+  case '4':
+    alert( 'Четыре' );
+    break;
+    case '5':
+        alert( 'Пять' );
+  default:
+    alert( 'Неизвестное значение' );
+}
